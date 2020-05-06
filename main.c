e5ba8952277ef0edb52b8341a16fa06a56285bea
KimGwangYong <46917994+brightdragom@users.noreply.github.com>
2020-05-06 11:08:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
+void plus(int x, int y);
+void minus(int x, int y);
+void multiplication(int x, int y);
+void divide(int x, int y);
+
 void main(){
   int x = 10;
   int y = 20;
   
+  plus(x, y);
+  minus(x, y);
+  multiplication(x, y);
+  divide(x, y);
   
   }
   
@@ -12,7 +21,8 @@ void plus(int x, int y){
 }
 
 void minus(int x, int y){
-  printf("x+y = %d", x-y);
+
+
 }
 
 void multiplication(int x, int y){
