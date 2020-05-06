c4111e4b9d366cff1f14b728c61b6a04c0f0900a
KimGwangYong <46917994+brightdragom@users.noreply.github.com>
2020-05-06 11:05:36 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main(){
+  int x = 10;
+  int y = 20;
+  
+  
+  }
+  
+void plus(int x, int y){
+
+}
+
+void minus(int x, int y){
+
+
+}
+
+void multiplication(int x, int y){
+
+
+}
+
+void divide(int x, int y){
+
+}
+  
